4d539983f3aa73476a443a138b743a346b8ddb6f
leiys13 <leiys13@icloud.com>
2020-12-07 01:46:46 +0800

    完成表单数据绑定

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,14 +5,31 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    name: '张三',
+    gender: [
+      {name: '男', value: 1, checked: true},
+      {name: '女', value: 2, checked: false}
+    ],
+    skills: [
+      {name: 'HTML', value: 'html', checked: true},
+      {name: 'CSS', value: 'css', checked: true},
+      {name: 'JavaScript', value: 'js', checked: false},
+      {name: 'Photoshop', value: 'ps', checked: false}
+    ],
+    opinion: '测试'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.request({
+      url: 'http://127.0.0.1:3000',
+      success: res => {
+        console.log(res)
+        this.setData(res.data)
+      }
+    })
   },
 
   /**
