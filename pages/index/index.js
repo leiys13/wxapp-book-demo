151e5a71417a5d8dfef6e7bf636bc3be08e46a98
leiys13 <leiys13@icloud.com>
2020-12-08 00:38:01 +0800

    计算器小程序示例

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    num: '123',
+    op: '+'
   },
 
   /**
@@ -62,5 +63,66 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  result: null,
+  isClear: false,
+  tapNumBtn: function (e) {
+    var num = e.target.dataset.val
+    if (this.data.num === '0' || this.isClear) {
+      this.setData({ num: num })
+      this.isClear = false
+    } else {
+      this.setData({ num: this.data.num + num })
+    }
+  },
+
+  tapOp: function (e) {
+    var op = this.data.op;
+    var num = Number(this.data.num)
+    this.setData({ op: e.target.dataset.val })
+    if (this.isClear) {
+      return
+    }
+    this.isClear = true
+    if (this.result === null) {
+      this.result = num
+      return
+    }
+    if (op === '+') {
+      this.result = this.result + num
+    } else if (op === '-') {
+      this.result = this.result - num
+    } else if (op === '*') {
+      this.result = this.result * num
+    } else if (op === '/') {
+      this.result = this.result / num
+    } else if (op === '%') {
+      this.result = this.result % num
+    }
+    this.setData({ num: this.result + '' })
+  },
+
+  tapDotBtn: function () {
+    if (this.isClear) {
+      this.setData({ num: '0.' })
+      this.isClear = false
+      return
+    }
+    if (this.data.num.indexOf('.') >=0) {
+      return
+    }
+    this.setData({ num: this.data.num + '.' })
+  },
+
+  tapDel: function () {
+    var num = this.data.num.substr(0, this.data.num.length - 1)
+    this.setData({ num: num === '' ? '0' : num })
+  },
+
+  tapReset: function () {
+    this.result = null
+    this.isClear = false
+    this.setData({ num: '0', op: '' })
   }
 })
