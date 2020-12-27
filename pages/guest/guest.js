7aea1cbd22fd67c13e141564cc20f594a06688b2
leiys13 <leiys13@icloud.com>
2020-12-27 19:19:58 +0800

    宾客信息页面

--- pages/guest/guest.js
+++ pages/guest/guest.js
@@ -5,7 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    picker: {
+      arr: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+      index: 1
+    }
   },
 
   /**
@@ -62,5 +65,54 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  changeNum: function (e) {
+    this.setData({
+      'picker.index': e.detail.value
+    })
+  },
+
+  blurName: function (e) {
+    this.checkName(e.detail.value)
+  },
+
+  checkName: function (d) {
+    var reg = /^[\u4E00-\u9FA5A-Za-z]+$/
+    return this.check(d, reg, '姓名输入有误')
+  },
+
+  blurPhone: function (e) {
+    this.checkPhone(e.detail.value)
+  },
+
+  checkPhone: function (d) {
+    var reg = /^(((13)|(15)|(17)|(18)|(19))\d{9})$/
+    return this.check(d, reg, '手机号码输入有误')
+  },
+
+  check: function (data, reg, errMsg) {
+    if (!reg.test(data)) {
+      wx.showToast({
+        title: errMsg,
+        icon: 'none',
+        duration: 1500
+      })
+      return false
+    }
+    return true
+  },
+
+  submitForm: function (e) {
+    var name = e.detail.value.name
+    var phone = e.detail.value.phone
+    if (this.checkName(name) && this.checkPhone(phone)) {
+      // 提交表单
+      wx.showToast({
+        title: '提交成功',
+        icon: 'success',
+        duration: 1500
+      })
+    }
   }
 })
