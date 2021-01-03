3f6ba2d7afcf43f5f2634d36ddd017979e1a008f
leiys13 <leiys13@icloud.com>
2021-01-03 17:58:27 +0800

    附近美食，调用腾讯位置服务，搜索附近餐厅

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,25 +1,76 @@
 // pages/index/index.js
+var QQMapWX = require('../../libs/qqmap-wx-jssdk.min.js')
 Page({
+
+  qqmapsdk: new QQMapWX({
+    key: 'CHYBZ-FEFKJ-CBEFE-K3JX7-L7HGT-5OFJD'
+  }),
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    mapw: '100%',
+    maph: '0',
+    scale: '18',
+    longitude: null,
+    latitude: null,
+    markers: null
   },
+
+  markIndex: 0,
+  mapCtx: null,
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.mapCtx = wx.createMapContext('map')
+    wx.getSystemInfo({
+      success: res => {
+        var mapw = res.windowWidth
+        var maph = res.windowHeight
+        this.setData({
+          maph: maph + 'px',
+          controls: [{
+            id: 1,
+            iconPath: '/images/center-point.png',
+            position: {
+              left: 10,
+              top: maph-50,
+              width: 30,
+              height: 30
+            },
+            clickable: true
+          }, {
+            id: 2,
+            iconPath: '/images/coupon.png',
+            position: {
+              left: mapw-60,
+              top: maph-50,
+              width: 30,
+              height: 30
+            },
+            clickable: true
+          }]
+        })
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    wx.getLocation({
+      type: 'gcj02',
+      success: res => {
+        this.setData({
+          longitude: res.longitude,
+          latitude: res.latitude
+        })
+      }
+    })
   },
 
   /**
@@ -62,5 +113,53 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  getFood: function (lng, lat) {
+    this.qqmapsdk.search({
+      keyword: '餐厅',
+      location: {
+        longitude: lng,
+        latitude: lat
+      },
+      success: res => {
+        console.log('getFood success:', res.data)
+        var mark = []
+        for (let i in res.data) {
+          mark.push({
+            iconPath: '/images/food.png',
+            id: i,
+            latitude: res.data[i].location.lat,
+            longitude: res.data[i].location.lng
+          })
+        }
+        mark.push({
+          iconPath: '/images/center-point.png',
+          id: res.data.length,
+          latitude: lat,
+          longitude: lng
+        })
+        this.setData({
+          markers: mark
+        })
+      }
+    })
+  },
+
+  tapControl: function (e) {
+    var id = e.controlId
+    if (id === 1) {
+      this.mapCtx.moveToLocation()
+    }
+  },
+
+  changeRegion: function (e) {
+    if (e.type === 'end') {
+      this.mapCtx.getCenterLocation({
+        success: res => {
+          this.getFood(res.longitude, res.latitude)
+        }
+      })
+    }
   }
 })
