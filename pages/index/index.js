8bff68d620c3a49675ff619fbe159884abc9a6a0
leiys13 <leiys13@icloud.com>
2021-01-09 12:04:55 +0800

    转盘动画

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,8 +5,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    animation: null
   },
+
+  animation: null,
+  angle: 0,
+  x1: 0,
+  y1: 0,
+  x3: 0,
+  y3: 0,
 
   /**
    * 生命周期函数--监听页面加载
@@ -26,7 +33,10 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.animation = wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'ease'
+    })
   },
 
   /**
@@ -62,5 +72,118 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  start: function (e) {
+    this.x1 = e.touches[0].clientX
+    this.y1 = e.touches[0].clientY
+  },
+
+  end: function (e) {
+    this.x3 = e.changedTouches[0].clientX
+    this.y3 = e.changedTouches[0].clientY
+    if (this.judgeturn(this.x1, this.y1, this.x3, this.y3)) {
+      this.angle = this.angle + 35
+      this.animation.rotate(this.angle).step()
+      this.setData({
+        animation: this.animation.export()
+      })
+    } else {
+      this.angle = this.angle - 30
+      this.animation.rotate(this.angle).step()
+      this.setData({
+        animation: this.animation.export()
+      })
+    }
+  },
+
+  tapRotate: function () {
+    this.animation.rotate(Math.random() * 720 - 360).step()
+    this.setData({
+      animation: this.animation.export()
+    })
+  },
+
+  tapScale: function () {
+    this.animation.scale(Math.random() * 2).step()
+    this.setData({
+      animation: this.animation.export()
+    })
+  },
+
+  tapTranslate: function () {
+    this.animation.translate(Math.random() * 100 - 50, Math.random() * 100 - 50).step()
+    this.setData({
+      animation: this.animation.export()
+    })
+  },
+
+  tapSkew: function () {
+    this.animation.skew(Math.random() * 45, Math.random() * 45).step()
+    this.setData({
+      animation: this.animation.export()
+    })
+  },
+
+  tapRotateAndScale: function () {
+    this.animation.rotate(Math.random() * 720 - 360)
+                  .scale(Math.random() * 2).step()
+    this.setData({
+      animation: this.animation.export()
+    })
+  },
+
+  tapRotateThenScale: function () {
+    this.animation.rotate(Math.random() * 720 - 360).step()
+                  .scale(Math.random() * 2).step()
+    this.setData({
+      animation: this.animation.export()
+    })
+  },
+
+  tapAll: function () {
+    this.animation.rotate(Math.random() * 720 - 360)
+                  .scale(Math.random() * 2)
+                  .translate(Math.random() * 100 - 50, Math.random() * 100 - 50)
+                  .skew(Math.random() * 45, Math.random() * 45).step()
+    this.setData({
+      animation: this.animation.export()
+    })
+  },
+
+  tapAllOrderBy: function () {
+    this.animation.rotate(Math.random() * 720 - 360).step()
+                  .scale(Math.random() * 2).step()
+                  .translate(Math.random() * 100 - 50, Math.random() * 100 - 50).step()
+                  .skew(Math.random() * 45, Math.random() * 45).step()
+    this.setData({
+      animation: this.animation.export()
+    })
+  },
+
+  tapReset1: function () {
+    this.animation.rotate(0)
+                  .scale(1)
+                  .translate(0, 0)
+                  .skew(0, 0).step()
+    this.setData({
+      animation: this.animation.export()
+    })
+  },
+
+  tapReset2: function () {
+    this.animation.skew(0, 0).step()
+                  .translate(0, 0).step()
+                  .scale(1).step()
+                  .rotate(0).step()
+    this.setData({
+      animation: this.animation.export()
+    })
+  },
+
+  judgeturn: function (x1, y1, x3, y3) {
+    var x2 = 150
+    var y2 = 150
+    return !((x2 - x1) * (y3 - y2) - (y2 - y1) * (x3 - x2) > 0)
   }
 })
