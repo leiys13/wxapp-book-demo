b0acc5d26516ac054c17bc39213a19436be038d0
leiys13 <leiys13@icloud.com>
2021-01-10 23:03:42 +0800

    使用websocket实现和服务器端在线聊天

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,14 +5,45 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    scrollTop: 0,
+    message: '',
+    newslist: []
   },
+
+  id: 0,
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this
 
+    wx.connectSocket({
+      url: 'ws://127.0.0.1:3000',
+    })
+
+    wx.onSocketOpen(function () {
+      console.log('WebSocket connect open')
+    })
+
+    wx.onSocketMessage(function (msg) {
+      console.log('receive msg:', msg)
+      var data = JSON.parse(msg.data)
+      data.role = 'server'
+      console.log('this.id:', that.id)
+      data.id = ++that.id
+      var list = that.data.newslist
+      list.push(data)
+      that.setData({
+        newslist: list,
+        message: null
+      })
+      that.rollingBottom()
+    })
+
+    wx.onSocketClose(function () {
+      console.log('WebSocket connect close')
+    })
   },
 
   /**
@@ -40,7 +71,8 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    wx.onSocketClose()
+    console.log('WebSocket connect close')
   },
 
   /**
@@ -62,5 +94,53 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  tapSend: function () {
+    if (this.data.message) {
+      wx.sendSocketMessage({
+        data: this.data.message
+      })
+      this.rollingBottom()
+      var list = []
+      list = this.data.newslist
+      var temp = {
+        id: this.id++,
+        content: this.data.message,
+        date: new Date(),
+        role: 'me'
+      }
+      list.push(temp)
+      this.setData({
+        newslist: list,
+        message: null
+      })
+      console.log('send: ', this.data.newslist)
+    } else {
+      wx.showToast({
+        title: '消息不能为空',
+        icon: 'none',
+        duration: 2000
+      })
+    }
+    this.rollingBottom()
+  },
+
+  inputChange: function (e) {
+    this.setData({
+      message: e.detail.value
+    })
+  },
+
+  rollingBottom: function () {
+    var s = 0
+    var list = wx.createSelectorQuery().selectAll('.list')
+    list.boundingClientRect(rects => {
+      rects.forEach( rect => {
+        this.setData({
+          scrollTop: rect.bottom
+        })
+      })
+    }).exec()
   }
 })
